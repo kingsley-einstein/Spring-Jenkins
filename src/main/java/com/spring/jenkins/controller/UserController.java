204228499c64f369e110798b98a804aee3ea8589
Kingsley Victor <javaprodigy56@gmail.com>
2019-07-08 09:36:38 -0700

    chore: update user controller

--- src/main/java/com/spring/jenkins/controller/UserController.java
+++ src/main/java/com/spring/jenkins/controller/UserController.java
@@ -1,8 +1,23 @@
 package com.spring.jenkins.controller;
 
+import java.util.List;
+
+import com.spring.jenkins.model.User;
+import com.spring.jenkins.repository.UserRepository;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping(value = "/api/v1")
-public class UserController {}
+public class UserController {
+    @Autowired
+    private UserRepository repo;
+
+    @GetMapping("/users")
+    public List<User> getUsers() {
+        return repo.findAll();
+    }
+}

d79e87057310b065220e7b3d9cc62419e62ad342
Kingsley Victor <javaprodigy56@gmail.com>
2019-07-08 09:51:20 -0700

    chore: update user controller

--- src/main/java/com/spring/jenkins/controller/UserController.java
+++ src/main/java/com/spring/jenkins/controller/UserController.java
@@ -7,6 +7,7 @@ import com.spring.jenkins.repository.UserRepository;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -19,5 +20,11 @@ public class UserController {
     @GetMapping("/users")
     public List<User> getUsers() {
         return repo.findAll();
+    }
+
+    @GetMapping("/user/{id}")
+    public User getUser(@PathVariable("id")Long id) {
+        return repo.findById(id)
+        .get();
     }
 }
